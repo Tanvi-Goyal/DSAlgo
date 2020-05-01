0251fcf49437612d98a3d8c0a43cca5c6378d3e6
tanvi <goyaltanvi94@gmail.com>
2020-05-01 12:15:53 +0530

    Palindrome Num | Max Sub Array | Move Zeroes

--- arrays/MainClass.java
+++ arrays/MainClass.java
@@ -4,26 +4,26 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-//		TwoSum twoSum = new TwoSum();
-//		System.out.print(twoSum.twoSum(new int[] {2, 7, 11, 15}, 9));
-//		
-//		SingleNumber singleNum = new SingleNumber();
-//		System.out.print(singleNum.singleNumber(new int[] {4,1,2,1,2}));
-//		
-//		ReverseInteger reverseInteger = new ReverseInteger();
-//		System.out.print(reverseInteger.reverse(123));
-//		
-//		HappyNumber happyNumber = new HappyNumber();
-//		System.out.print(happyNumber.isHappy(19));
-//		
-//		PalindromeNumber palindromeNumber = new PalindromeNumber();
-//		System.out.print(palindromeNumber.isPalindrome(121));
-//		
-//		MaximumSubArray maxSubArray = new MaximumSubArray();
-//		System.out.print(maxSubArray.maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,4}));
+		TwoSum twoSum = new TwoSum();
+		System.out.print(twoSum.twoSum(new int[] {2, 7, 11, 15}, 9));
+		
+		SingleNumber singleNum = new SingleNumber();
+		System.out.print(singleNum.singleNumber(new int[] {4,1,2,1,2}));
+		
+		ReverseInteger reverseInteger = new ReverseInteger();
+		System.out.print(reverseInteger.reverse(123));
+		
+		HappyNumber happyNumber = new HappyNumber();
+		System.out.print(happyNumber.isHappy(19));
+		
+		PalindromeNumber palindromeNumber = new PalindromeNumber();
+		System.out.print(palindromeNumber.isPalindrome(121));
+		
+		MaximumSubArray maxSubArray = new MaximumSubArray();
+		System.out.print(maxSubArray.maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,4}));
 //		
 		MoveZeroes moveZeroes = new MoveZeroes();
-		System.out.print(moveZeroes.moveZeroes(new int[] {0,1,0,3,12}));
+		moveZeroes.moveZeroes(new int[] {0,1,0,3,12});
 	}
 
 }
