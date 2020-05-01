e32808b582c67798a38d287aeab79ebb3f4f937e
tanvi <goyaltanvi94@gmail.com>
2020-05-01 18:25:30 +0530

    Contains Duplicate

--- arrays/App.java
+++ arrays/App.java
@@ -94,6 +94,9 @@ public class App {
 		RotateArray rotateArray = new RotateArray();
 		rotateArray.rotate(new int[] {1,2,3,4,5,6,7},3);
 		
+		ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+		System.out.println(containsDuplicate.containsDuplicate(new int[] {1,1,1,3,3,4,3,2,4,2}));
+		
 	}
 
 }
