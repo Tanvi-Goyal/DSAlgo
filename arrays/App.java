eeaa1606169fc55a105a2cd583170189bf825fd7
tanvi <goyaltanvi94@gmail.com>
2020-05-15 18:24:25 +0530

    Permutations

--- arrays/App.java
+++ arrays/App.java
@@ -148,6 +148,9 @@ public class App {
 		
 		NextGreaterPermutation nextGreaterPermutation = new NextGreaterPermutation();
 		
+		Permutations permutations = new Permutations();
+		System.out.println(permutations.permute(new int[] {1,2,3}));
+		
 	}
 
 }
---
+++ arrays/Permutations.java
@@ -0,0 +1,50 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+
+	 public List<List<Integer>> permute(int[] nums) {
+	        
+	        if(nums.length == 0) {
+	            List<List<Integer>> res = new ArrayList<List<Integer>>();
+	            List<Integer> list = new ArrayList<>();
+	            res.add(list);
+	            return res;
+	        }
+	        
+	        int currentVal = nums[0];
+	        int[] subArray = new int[nums.length - 1];
+	        for(int i=1;i< nums.length;i++) {
+	            subArray[i-1] = nums[i];
+	        }
+	        
+	        List<List<Integer>> subRes = permute(subArray);
+	        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+	        for(int i=0; i < subRes.size();i++) {
+	        	
+	            List<Integer> permutation = subRes.get(i);     
+	            for(int j = 0;j < permutation.size(); j++) {
+	            	
+		            List<Integer> newList = new ArrayList<Integer>();     
+		            newList.addAll(permutation);
+		            newList.add(j, currentVal);
+	            	ans.add(newList);
+//					ans[k] = smallAns[i].substring(0,j) + input.charAt(0) + smallAns[i].substring(j);
+//					k++;
+				}
+	            
+	            List<Integer> newList = new ArrayList<Integer>();     
+	            newList.addAll(permutation);
+	            newList.add(currentVal);
+            	ans.add(newList);
+            	
+//				ans[k] = smallAns[i] + input.charAt(0);
+//				k++;
+	            
+	        }
+	        
+	        return ans;
+	    }
+}
