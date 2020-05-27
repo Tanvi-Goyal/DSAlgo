a23c65c612708d6168b781250967970c9afbe77e
tanvi <goyaltanvi94@gmail.com>
2020-05-27 10:30:51 +0530

    valid sudoku

--- arrays/ValidSudoko.java
+++ arrays/ValidSudoko.java
@@ -6,6 +6,16 @@ public class ValidSudoko {
 
 	public boolean isValidSudoko(char board[][]) {
 		
+		/*
+		 Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated 
+		 according to the following rules:
+
+	 	 Each row must contain the digits 1-9 without repetition.
+	  	 Each column must contain the digits 1-9 without repetition.
+		 Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+		 */
+		
 		for(int i = 0; i < 9; i++) {
 			HashSet<Character> rows = new HashSet<Character>();
 	        HashSet<Character> columns = new HashSet<Character>();
