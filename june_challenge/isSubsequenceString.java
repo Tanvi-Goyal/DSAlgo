663890ee89f73fe882081a340f144b8077d44235
tanvi <goyaltanvi94@gmail.com>
2020-06-09 23:14:04 +0530

    Is subsequence - Day 9

--- june_challenge/isSubsequenceString.java
+++ june_challenge/isSubsequenceString.java
@@ -12,8 +12,9 @@ public class isSubsequenceString {
 	 */
 	
 	public boolean isSubsequence(String s, String t) {
-        if (s.length() == 0) return true;
+       if (s.length() == 0) return true;
        int indexS = 0, indexT = 0;
+       
        while (indexT < t.length()) {
            if (t.charAt(indexT) == s.charAt(indexS)) {
                indexS++;
