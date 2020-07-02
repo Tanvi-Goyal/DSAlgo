31532ae323c99f157fe62f980da8c112e8305370
tanvi <goyaltanvi94@gmail.com>
2020-07-02 23:47:43 +0530

    Level order traversal - Day 2

--- july_challenge/App.java
+++ july_challenge/App.java
@@ -8,6 +8,17 @@ public class App {
 		// Day 1
 		ArrangeCoins arrangeCoins = new ArrangeCoins();
 		System.out.println(arrangeCoins.arrangeCoins(5));
+		
+		// Day 2
+		BTLevelOrder BTLevelOrder = new BTLevelOrder();
+		System.out.println(BTLevelOrder.levelOrderBottom(
+				new TreeNode(3, 
+						new TreeNode(9),
+						new TreeNode(20,
+								new TreeNode(15),
+								new TreeNode(7)
+								)
+						)));
 	}
 
 }
